0ddff3553bc6286fc50eea5fbc63af6358f5ae8f
Abdul-bari-Zahid <bariblogger6@gmail.com>
2024-09-27 00:34:17 -0700

    done

---
+++ scripts8.js
@@ -0,0 +1,9 @@
+// var arr = [2,4,6,8,10,12,14,16,18,20]
+// for( i=2 ; i>arr.length; i++){
+//     console.log(arr[i])
+// }
+
+var userinput = +prompt("Enter A Value")
+for(i=1; i<11; i++){
+    document.write(userinput+" " +"x"+" "+i+" "+" "+"="+" "+userinput*i ,"</br>")
+}
